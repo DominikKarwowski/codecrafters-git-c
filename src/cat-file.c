6bcd4659c493fa16de0fffbc96f7b49b045f8d11
DominikKarwowski <karwowski.d@o2.pl>
2025-03-05 19:27:03 +0100

    Add debug helpers

--- src/cat-file.c
+++ src/cat-file.c
@@ -1,12 +1,12 @@
 #include "cat-file.h"
+#include "debug_helper.h"
 
 #include <assert.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#include "zlib.h"
+#include <zlib.h>
 
 #define CHUNK 65536
 
@@ -42,6 +42,7 @@ bool try_resolve_cat_file_opts(const int argc, char **argv)
     bool pretty_print = false;
     bool show_type = false;
     bool show_size = false;
+    bool set_result;
 
     int opt;
     while ((opt = getopt(argc, argv, "tps")) != -1)
@@ -49,32 +50,28 @@ bool try_resolve_cat_file_opts(const int argc, char **argv)
         switch (opt)
         {
             case 't':
-                if (!try_set_cat_file_opt(&show_type, "-t", pretty_print, "-p", show_size, "-s"))
-                {
-                    return false;
-                }
+                set_result = try_set_cat_file_opt(&show_type, "-t", pretty_print, "-p", show_size, "-s");
+                validate(set_result, "Failed to set 't' option.");
                 break;
             case 'p':
-                if (!try_set_cat_file_opt(&pretty_print, "-p", show_type, "-t", show_size, "-s"))
-                {
-                    return false;
-                }
+                set_result = try_set_cat_file_opt(&pretty_print, "-p", show_type, "-t", show_size, "-s");
+                validate(set_result, "Failed to set 'p' option.");
                 break;
             case 's':
-                if (!try_set_cat_file_opt(&show_size, "-s", show_type, "-t", pretty_print, "-p"))
-                {
-                    return false;
-                }
+                set_result = try_set_cat_file_opt(&show_size, "-s", show_type, "-t", pretty_print, "-p");
+                validate(set_result, "Failed to set 's' option.");
                 break;
             case '?':
-                printf("Invalid switch: '%c'\n", optopt);
-                break;
+                validate(false, "Invalid switch: '%c'\n", optopt);
             default:
-                fprintf(stderr, "Unrecognized option: %c\n", optopt);
+                validate(false, "Unrecognized option: %c\n", optopt);
         }
     }
 
     return true;
+
+error:
+    return false;
 }
 
 static struct object_path get_obj_path(const char *obj_hash)
@@ -101,7 +98,7 @@ static struct object_path get_obj_path(const char *obj_hash)
     return obj_path;
 }
 
-int print_inflate_result(FILE *source, FILE *dest)
+void print_inflate_result(FILE *source, FILE *dest)
 {
     z_stream infstream = {
         .zalloc = Z_NULL,
@@ -112,21 +109,14 @@ int print_inflate_result(FILE *source, FILE *dest)
     };
 
     int ret = inflateInit(&infstream);
-    if (ret != Z_OK)
-    {
-        return ret;
-    }
+    validate(ret == Z_OK, "Failed to initialize inflate.");
 
     do
     {
         unsigned char in[CHUNK];
         infstream.avail_in = fread(in, 1, CHUNK, source);
 
-        if (ferror(source))
-        {
-            (void)inflateEnd(&infstream);
-            return Z_ERRNO;
-        }
+        validate(ferror(source) == 0, "Failed to read source data.");
 
         if (infstream.avail_in == 0)
         {
@@ -150,33 +140,33 @@ int print_inflate_result(FILE *source, FILE *dest)
                     ret = Z_DATA_ERROR; /* and fall through */
                 case Z_DATA_ERROR:
                 case Z_MEM_ERROR:
-                    (void)inflateEnd(&infstream);
-                    return ret;
+                    validate(false, "Failed to inflate with Z error code: %d.", ret);
             }
 
             const unsigned have = CHUNK - infstream.avail_out;
-            if (fwrite(out, 1, have, dest) != have || ferror(dest))
-            {
-                (void)inflateEnd(&infstream);
-                return Z_ERRNO;
-            }
+
+            const size_t write_size = fwrite(out, 1, have, dest);
+
+            validate(write_size == have, "Failed writing to output stream.");
 
         } while (infstream.avail_out == 0);
 
     } while (ret != Z_STREAM_END);
 
     (void)inflateEnd(&infstream);
-    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+
+    validate(ret == Z_STREAM_END, "Failed to inflate with Z error code: %d", Z_DATA_ERROR);
+    return;
+
+error:
+    (void)inflateEnd(&infstream);
 }
 
 int cat_file(const int argc, char *argv[])
 {
     const char *obj_hash = argv[3];
 
-    if (!try_resolve_cat_file_opts(argc, argv))
-    {
-        return 1;
-    }
+    validate(try_resolve_cat_file_opts(argc, argv), "Failed to resolve options.");
 
     struct object_path obj_path = get_obj_path(obj_hash);
 
@@ -185,14 +175,14 @@ int cat_file(const int argc, char *argv[])
 
     FILE *obj_file = fopen(full_obj_path, "r");
 
-    if (!obj_file)
-    {
-        return 1;
-    }
+    validate(obj_file, "Failed to open object file.");
 
     print_inflate_result(obj_file, stdout);
 
     fclose(obj_file);
 
     return 0;
+
+error:
+    return 1;
 }
---
+++ src/debug_helper.h
@@ -0,0 +1,22 @@
+#ifndef DEBUG_HELPER_H
+#define DEBUG_HELPER_H
+
+#include <errno.h>
+#include <string.h>
+
+#define errno_desc() \
+    (errno == 0 ? "No error number" : strerror(errno))
+
+#define log_error(M, ...) \
+    fprintf(stderr,\
+        "%s:%d: errno: %s " M "\n", \
+        __FILE__, __LINE__, errno_desc(), ##__VA_ARGS__)
+
+#define validate(A, M, ...) \
+({  __typeof__ (A) a__type_of = (A); \
+    if(!a__type_of) { \
+        log_error(M, ##__VA_ARGS__); \
+        errno = 0; \
+        goto error; } })
+
+#endif //DEBUG_HELPER_H
